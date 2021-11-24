f6b40a8250d8039ca26b19fd2603f63252eaf0a9
Kristofer <kristofer.agnulf@iths.se>
2021-11-24 11:36:14 +0100

    Add baic repo structure

--- Backend/DagnysBageri/Repositories/UserRepository.cs
+++ Backend/DagnysBageri/Repositories/UserRepository.cs
@@ -1,18 +1,39 @@
-﻿using System;
+﻿using DagnysBageri.Data;
+using DagnysBageri.Interfaces;
+using DagnysBageri.Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using DagnysBageri.Data;
 
 namespace DagnysBageri.Repositories
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
     {
         private readonly UserContext _context;
 
         public UserRepository(UserContext context)
         {
             _context = context;
+        }
+
+        public Task<bool> AddUserAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveUserAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateUserAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<User>> ListAllUsersAsync()
+        {
+            throw new NotImplementedException();
         }
     }
 }
