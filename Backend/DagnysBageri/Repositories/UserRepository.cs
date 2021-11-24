22d3a1efba50a3fc80fb489f96ed4304b29e16c5
Patrick Holst Roghell <77493439+PatrickHolst@users.noreply.github.com>
2021-11-24 13:31:05 +0100

    Puttes Repository

--- Backend/DagnysBageri/Repositories/UserRepository.cs
+++ Backend/DagnysBageri/Repositories/UserRepository.cs
@@ -1,39 +1,67 @@
 ﻿using DagnysBageri.Data;
 using DagnysBageri.Interfaces;
 using DagnysBageri.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace DagnysBageri.Repositories
 {
-    public class UserRepository : IUserRepository
+  public class UserRepository : IUserRepository
+  {
+    private readonly UserContext _context;
+
+    public UserRepository(UserContext context)
     {
-        private readonly UserContext _context;
-
-        public UserRepository(UserContext context)
-        {
-            _context = context;
-        }
-
-        public Task<bool> AddUserAsync(User user)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<bool> RemoveUserAsync(User user)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<bool> UpdateUserAsync(User user)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IList<User>> ListAllUsersAsync()
-        {
-            throw new NotImplementedException();
-        }
+      _context = context;
     }
+
+    public async Task<bool> AddUserAsync(User user)
+    {
+      try
+      {
+        await _context.Users.AddAsync(user);
+        return true;
+      }
+      catch
+      {
+        return false;
+
+      }
+    }
+
+    public async Task<bool> RemoveUserAsync(User user)
+    {
+      try
+      {
+        _context.Remove(user);
+        return await Task.FromResult<bool>(true);
+      }
+      catch
+      {
+        return await Task.FromResult<bool>(false);
+
+      }
+    }
+
+    public async Task<bool> UpdateUserAsync(User user)
+    {
+      try
+      {
+        _context.Update(user);
+        return await Task.FromResult<bool>(true);
+      }
+      catch
+      {
+        return await Task.FromResult<bool>(false);
+
+      }
+    }
+
+    public async Task<IList<User>> ListAllUsersAsync()
+    {
+      return await _context.Users.ToListAsync();
+    }
+  }
 }
