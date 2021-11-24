b2b590738c31795307978ded77ce9edf71513c7e
MarkusJohansson23 <77443853+MarkusJohansson23@users.noreply.github.com>
2021-11-24 11:24:38 +0100

    Created Interfaces

---
+++ Backend/DagnysBageri/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DagnysBageri.Models;
+
+namespace DagnysBageri.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<bool> AddUserAsync(User user);
+        Task<bool> RemoveUserAsync(User user);
+        Task<bool> UpdateUserAsync (User user);
+        Task<IList<User>> ListAllUsersAsync();
+    }
+}
